055ad5d1402dc0050925da5e8e6f4fffb2802457
gary0413 <zxc830414@gmail.com>
2026-07-02 02:59:31 +0800

    cask: use atomic FileUtils.rm_f

--- Casks/saykey.rb
+++ Casks/saykey.rb
@@ -37,7 +37,7 @@ cask "saykey" do
                      args: ["-L", "--fail", "--progress-bar", "-o", tmp, file_url]
       got_sha = Digest::SHA256.file(tmp).hexdigest
       if got_sha != want_sha
-        File.delete(tmp) if File.exist?(tmp)
+        FileUtils.rm_f(tmp)
         odie "SayKey model #{name} failed checksum (expected #{want_sha}, got #{got_sha})"
       end
       FileUtils.mv(tmp, dest)
